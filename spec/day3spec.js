3eb8c35f61d22006f8a28cb6f1c825e785ecbee8
Christian Clausen <maxclausen@gmail.com>
2020-12-03 21:03:48 +0100

    Run d3e2

--- spec/day3spec.js
+++ spec/day3spec.js
@@ -1,9 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var day3_1 = require("../day3");
-it("works with given example", function () {
-    var map = "..##.......\n  #...#...#..\n  .#....#..#.\n  ..#.#...#.#\n  .#...##..#.\n  ..#.##.....\n  .#.#.#....#\n  .#........#\n  #.##...#...\n  #...##....#\n  .#..#...#.#"
-        .split("\n")
-        .map(function (x) { return x.split(""); });
-    expect(day3_1.ex1(map)).toBe(7);
+describe("Exercise 1", function () {
+    it("works with given example", function () {
+        var map = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#"
+            .split("\n")
+            .map(function (x) { return x.trim().split(""); });
+        expect(day3_1.ex1(map)).toBe(7);
+    });
+});
+describe("Exercise 2", function () {
+    it("works with given example", function () {
+        var map = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#"
+            .split("\n")
+            .map(function (x) { return x.trim().split(""); });
+        expect(day3_1.ex2(map)).toBe(336);
+    });
 });
