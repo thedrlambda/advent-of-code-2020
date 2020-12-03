a82a3c25c93520f2e19dcb16df0b247b1bf4ebfb
Christian Clausen <maxclausen@gmail.com>
2020-12-03 19:48:34 +0100

    Implement core functionality

--- day1.js
+++ day1.js
@@ -2,6 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ex1 = void 0;
 function ex1(input) {
+    for (var i = 0; i < input.length - 1; i++)
+        for (var j = i + 1; j < input.length; j++)
+            if (input[i] + input[j] === 2020) {
+                return input[i] * input[j];
+            }
     return 514579;
 }
 exports.ex1 = ex1;
--- day1.ts
+++ day1.ts
@@ -1,3 +1,8 @@
 export function ex1(input: number[]): number {
+  for (let i = 0; i < input.length - 1; i++)
+    for (let j = i + 1; j < input.length; j++)
+      if (input[i] + input[j] === 2020) {
+        return input[i] * input[j];
+      }
   return 514579;
 }
