bfb5b621a65b13717a0cb557aa908e0fd22d78cd
Christian Clausen <maxclausen@gmail.com>
2020-12-03 20:27:00 +0100

    Run d2e1

--- day2.js
+++ day2.js
@@ -1,6 +1,26 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ex1core = void 0;
+exports.driver = exports.ex1core = void 0;
+var fs = __importStar(require("fs"));
 function isValid(conf) {
     var count = 0;
     for (var i = 0; i < conf.password.length; i++) {
@@ -19,3 +39,19 @@ function ex1core(input) {
     return valid;
 }
 exports.ex1core = ex1core;
+function parse(line) {
+    var parts = line.split(": ");
+    var policyParts = parts[0].split(" ");
+    var rangeParts = policyParts[0].split("-");
+    return {
+        policy: { min: +rangeParts[0], max: +rangeParts[1], char: policyParts[1] },
+        password: parts[1],
+    };
+}
+function driver(readFile, output) {
+    var content = readFile();
+    var input = content.split("\n").map(parse);
+    output("" + ex1core(input));
+}
+exports.driver = driver;
+driver(function () { return "" + fs.readFileSync("ex2.txt"); }, function (s) { return console.log(s); });
--- day2.ts
+++ day2.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+
 function isValid(conf: {
   policy: { min: number; max: number; char: string };
   password: string;
@@ -24,3 +26,24 @@ export function ex1core(
     }
   return valid;
 }
+
+function parse(line: string) {
+  let parts = line.split(": ");
+  let policyParts = parts[0].split(" ");
+  let rangeParts = policyParts[0].split("-");
+  return {
+    policy: { min: +rangeParts[0], max: +rangeParts[1], char: policyParts[1] },
+    password: parts[1],
+  };
+}
+
+export function driver(readFile: () => string, output: (_: string) => void) {
+  let content = readFile();
+  let input = content.split("\n").map(parse);
+  output("" + ex1core(input));
+}
+
+driver(
+  () => "" + fs.readFileSync("ex2.txt"),
+  (s) => console.log(s)
+);
